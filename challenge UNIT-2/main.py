bd10e56dc012311cbb97e21452fbc45bf2339814
n114 <149319135+n114@users.noreply.github.com>
2023-10-29 22:52:03 +0530

    Add files via upload

--- challenge UNIT-2/main.py
+++ challenge UNIT-2/main.py
@@ -1,27 +1,23 @@
-class BankAccount:
-    def __init__(self, balance=0):
-        self.balance = balance
+# Define the Batsman class
+class Batsman:
+    def __init__(self, name):
+        self.name = name
 
-    def deposit(self, amount):
-        if amount > 0:
-            self.balance += amount
-            return f"Deposited ${amount}. New balance: ${self.balance}"
-        else:
-            return "Invalid deposit amount."
+    def play(self):
+        print(f"{self.name} is batting.")
 
-    def withdraw(self, amount):
-        if 0 < amount <= self.balance:
-            self.balance -= amount
-            return f"Withdrew ${amount}. New balance: ${self.balance}"
-        else:
-            return "Invalid withdrawal amount or insufficient funds."
+# Define the Bowler class
+class Bowler:
+    def __init__(self, name):
+        self.name = name
 
-# Create an instance of BankAccount
-account = BankAccount()
+    def play(self):
+        print(f"{self.name} is bowling.")
 
-# Test deposit functionality
-print(account.deposit(100))  # Deposited $100. New balance: $100
+# Create objects of the Batsman and Bowler classes
+batsman1 = Batsman("Sachin")
+bowler1 = Bowler("Warne")
 
-# Test withdrawal functionality
-print(account.withdraw(50))   # Withdrew $50. New balance: $50
-print(account.withdraw(75))   # Invalid withdrawal amount or insufficient funds.
+# Call the play() method for each object
+batsman1.play()  # Sachin is batting.
+bowler1.play()   # Warne is bowling.
